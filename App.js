073de177e655ae5e02148f9aece6756374bb321b
muhammadsarim555 <muhammadsarim555@gmail.com>
2019-05-05 02:51:26 +0500

    Google Facebook Login Done

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import FacebookBtn from './src/facebook'
+import GoogleBtn from './src/google'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <FacebookBtn />
+        <GoogleBtn />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
